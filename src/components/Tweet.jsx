ed62a9eaa41081fa96ad3d43fe0404dcac82c527
John Doe <marrian1997@hotmail.com>
2023-08-21 16:09:24 +0100

    solved

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,39 +1,30 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+import Actions from "./Actions";
+import Message from "./Message";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import ProfileImage from "./ProfileImage";
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
-
-        <p className="message">
+function Tweet(props) {
+  <p className="message">
           On December 7th, we will be hosting a #webinar that will introduce you
           to #SQL! Are you ready? 🚀
         </p>
+         const {tweet: {user: image, name, handle}, timestamp, message} = props
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+return (
+  <div className="tweet">
+    <ProfileImage image={image}/>
+    <div className="body">
+      <div className="top">
+        <User name={name} handle={handle} />
+        <Timestamp timestamp={timestamp} />
       </div>
-
-      <i className="fas fa-ellipsis-h"></i>
+      <Message message={message} />
+      <Actions />
     </div>
-  );
-}
 
+    <i className="fas fa-ellipsis-h"></i>
+  </div>
+);
+}
 export default Tweet;
